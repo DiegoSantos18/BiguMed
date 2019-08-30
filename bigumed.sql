fb87a3477fae384baad6cf181674ff76f4340308
Henrique Müller <henrique.dario.muller@gmail.com>
2019-08-30 20:52:51 -0300

    Add files via upload

--- bigumed.sql
+++ bigumed.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 30-Ago-2019 às 06:42
--- Versão do servidor: 10.1.31-MariaDB
--- PHP Version: 7.2.4
+-- Generation Time: 31-Ago-2019 às 01:52
+-- Versão do servidor: 10.1.37-MariaDB
+-- versão do PHP: 7.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -61,21 +61,36 @@ INSERT INTO `medico` (`cpf`, `crm`, `especialidade`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `paciente`
+--
+
+CREATE TABLE `paciente` (
+  `cpf` char(11) CHARACTER SET utf8 NOT NULL,
+  `rg` varchar(25) NOT NULL,
+  `nroconvenio` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `usuario`
 --
 
 CREATE TABLE `usuario` (
   `cpf` char(11) NOT NULL,
   `senha` varchar(20) NOT NULL,
-  `nomecompleto` varchar(70) NOT NULL
+  `nomecompleto` varchar(70) NOT NULL,
+  `tipo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `usuario`
 --
 
-INSERT INTO `usuario` (`cpf`, `senha`, `nomecompleto`) VALUES
-('04053085071', '12345', 'Diego Santos');
+INSERT INTO `usuario` (`cpf`, `senha`, `nomecompleto`, `tipo`) VALUES
+('03500088090', '123456', 'Henrique Dario Müller', 2),
+('04053085071', '12345', 'Diego Santos', 1),
+('67136049008', '987654321', 'Fulano Ciclano', 3);
 
 --
 -- Indexes for dumped tables
@@ -95,6 +110,12 @@ ALTER TABLE `medico`
   ADD UNIQUE KEY `cpf` (`cpf`);
 
 --
+-- Indexes for table `paciente`
+--
+ALTER TABLE `paciente`
+  ADD PRIMARY KEY (`cpf`);
+
+--
 -- Indexes for table `usuario`
 --
 ALTER TABLE `usuario`
@@ -110,6 +131,12 @@ ALTER TABLE `usuario`
 --
 ALTER TABLE `medico`
   ADD CONSTRAINT `medico_ibfk_1` FOREIGN KEY (`cpf`) REFERENCES `usuario` (`cpf`);
+
+--
+-- Limitadores para a tabela `paciente`
+--
+ALTER TABLE `paciente`
+  ADD CONSTRAINT `paciente_ibfk_1` FOREIGN KEY (`cpf`) REFERENCES `usuario` (`cpf`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
